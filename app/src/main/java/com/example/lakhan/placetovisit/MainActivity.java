cb7d0cb825042f295b55ae8bd0fe5f7a59c52317
FinalOne <lakhanrathi65@gmail.com>
2019-02-09 19:15:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lakhan/placetovisit/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.lakhan.placetovisit;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    static ArrayList<String> places;
+    static ArrayList<LatLng> location;
+   static ArrayAdapter arrayAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getActionBar();
+
+
+        ListView listView = (ListView) findViewById(R.id.placeList);
+
+         places = new ArrayList<String>();
+        places.add("ADD NEW PLACE");
+        location = new ArrayList<LatLng>();
+        location.add(new LatLng(0,0));
+
+         arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,places);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(getApplicationContext(),MapsActivity.class);
+                i.putExtra("Info",position);
+                startActivity(i);
+
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
